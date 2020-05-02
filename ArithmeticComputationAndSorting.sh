d187a75e5d310358953a36b50ec718b99498a7c7
Srijan-18 <srijan.mishrasir@gmail.com>
2020-05-02 05:49:40 +0530

    UC4 implemented

--- ArithmeticComputationAndSorting.sh
+++ ArithmeticComputationAndSorting.sh
@@ -4,5 +4,7 @@ read -p "Enter Value of variable a :" a
 read -p "Enter Value of variable b :" b
 read -p "Enter Value of variable c :" c
 
-result1=((a+b*c))
-result2=((a*b+c))
+result1=$((a+b*c))
+result2=$((a*b+c))
+result3=$(bc <<< "scale=4; $c+$a/$b ")
+
