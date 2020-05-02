303a55d4d5bc30147202aba9cff4f2dbfdbde491
Srijan-18 <srijan.mishrasir@gmail.com>
2020-05-02 05:59:11 +0530

    UC6 results stored in Dictionary

--- ArithmeticComputationAndSorting.sh
+++ ArithmeticComputationAndSorting.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -x
-
+declare -A result
 read -p "Enter Value of variable a :" a
 read -p "Enter Value of variable b :" b
 read -p "Enter Value of variable c :" c
 
-result1=$((a+b*c))
-result2=$((a*b+c))
-result3=$(bc <<< "scale=4; $c+$a/$b ")
-result4=$((a%b+c))
+result[UC2]=$((a+b*c))
+result[UC3]=$((a*b+c))
+result[UC4]=$(bc <<< "scale=4; $c+$a/$b ")
+result[UC5]=$((a%b+c))
