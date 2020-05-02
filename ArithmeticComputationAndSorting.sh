5e510ae2ccb5df46743af2929d21134ef4d4d8e6
Srijan-18 <srijan.mishrasir@gmail.com>
2020-05-02 06:35:01 +0530

    UC8 array sorted in descending order

--- ArithmeticComputationAndSorting.sh
+++ ArithmeticComputationAndSorting.sh
@@ -6,11 +6,26 @@ read -p "Enter Value of variable c :" c
 
 result[UC2]=$((a+b*c))
 result[UC3]=$((a*b+c))
-result[UC4]=$(bc <<< "scale=4; $c+$a/$b ")
+result[UC4]=$(( c+a/b ))
 result[UC5]=$((a%b+c))
 arrayIndex=0
 for key in ${!result[@]}
 do
 	resultArray[((arrayIndex++))]=${result[$key]}
 done
-echo ${resultArray[@]}
+#bubble Sorting in descending order. i & j are loop variables
+
+for((i=0;i<arrayIndex-1;i++))
+do
+	for((j=0;j<arrayIndex-1-i;j++))
+	do
+		if [ ${resultArray[$j]} -lt ${resultArray[$j+1]} ]
+		then
+			temp=${resultArray[$j]}
+			resultArray[$j]=${resultArray[$j+1]}
+			resultArray[$j+1]=$temp
+		fi
+	done
+done
+
+
